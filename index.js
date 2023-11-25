67e2a3cc14de4af69f099e64ac861cb89844cc86
Mahfuja Ahsan Muna <121941219+mahfuja5768@users.noreply.github.com>
2023-11-25 16:36:07 +0600

    add jwt

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
-
-
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(process.env.DB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("realEstate").collection("users");
+
+    //jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res.send({ token });
+    });
+
+    //post users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (!existingUser) {
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      } else {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -34,18 +62,10 @@ run().catch(console.dir);
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("Dream property is running....");
-  });
-  
-  app.listen(port, (req, res) => {
-    console.log(`Dream property is running on ${port}`);
-  });
+  res.send("Dream property is running....");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Dream property is running on ${port}`);
+});
