8beb2af7f52ac521aa359dca27726fb09d4dc11e
Mahfuja Ahsan Muna <121941219+mahfuja5768@users.noreply.github.com>
2023-11-26 13:31:18 +0600

    add offer property route

--- index.js
+++ index.js
@@ -23,14 +23,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const usersCollection = client.db("realEstate").collection("users");    
-    const propertiesCollection = client.db("realEstate").collection("properties");    
+    const usersCollection = client.db("realEstate").collection("users");
+    const propertiesCollection = client
+      .db("realEstate")
+      .collection("properties");
     const wishlistCollection = client.db("realEstate").collection("wishlists");
     const reviewsCollection = client.db("realEstate").collection("reviews");
-
-
-
-
+    const offerPropertiesCollection = client
+      .db("realEstate")
+      .collection("offerProperties");
 
     //jwt
     app.post("/jwt", async (req, res) => {
@@ -52,29 +53,39 @@ async function run() {
       } else {
         return res.send({ message: "user already exists", insertedId: null });
       }
-    }); 
-    
-    
-    //get properties
-    app.get("/users", async (req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result)
     });
 
+    //get users
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
+    //update users
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          photoUrl: updateUser.photoUrl,
+        },
+      };
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
-
-    //post properties
+    //add properties for agent
     app.post("/properties", async (req, res) => {
       const property = req.body;
       const result = await propertiesCollection.insertOne(property);
-      res.send(result)
-    });  
-    
+      res.send(result);
+    });
+
     //get properties
     app.get("/properties", async (req, res) => {
       const result = await propertiesCollection.find().toArray();
-      res.send(result)
+      res.send(result);
     });
 
     //get single properties
@@ -89,16 +100,43 @@ async function run() {
       }
     });
 
-      //add review
-      app.post("/reviews", async (req, res) => {
-        try {
-          const review = req.body;
-          const result = await reviewsCollection.insertOne(review);
-          res.send(result);
-        } catch (error) {
-          console.log(error);
+    //add review
+    app.post("/reviews", async (req, res) => {
+      try {
+        const review = req.body;
+        review.timestamp = Date.now();
+        const result = await reviewsCollection.insertOne(review);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    //get wishlist Blogs
+    app.get("/reviews", async (req, res) => {
+      try {
+        let query = {};
+        if (req.query?.email) {
+          query = { email: req.query.email };
         }
-      });
+        const result = await reviewsCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    //delete reviews
+    app.delete("/reviews/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await reviewsCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
     //add to wishlist
     app.post("/wishlists", async (req, res) => {
@@ -111,7 +149,7 @@ async function run() {
       }
     });
 
-    //get wishlist Blogs
+    //get wishlist properties
     app.get("/wishlists", async (req, res) => {
       try {
         let query = {};
@@ -125,7 +163,70 @@ async function run() {
       }
     });
 
+    //delete wishlist property
+    app.delete("/wishlists/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await propertiesCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    app.delete("/offerProperties/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await offerPropertiesCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
+    //offer for property
+    app.post("/offerProperties", async (req, res) => {
+      try {
+        const property = req.body;
+        property.date = Date.now();
+        property.status = "pending";
+        const result = await offerPropertiesCollection.insertOne(property);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    //get offer properties for users
+    app.get("/offerPropertiesByBuyerEmail", async (req, res) => {
+      try {
+        const buyerEmail = req.query.buyerEmail;
+        const properties = await offerPropertiesCollection
+          .find({
+            buyerEmail: buyerEmail,
+          })
+          .toArray();
+
+        res.send(properties);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    //get offer properties for agent
+    app.get("/offerProperties", async (req, res) => {
+      try {
+        let query = {};
+        if (req.query?.email) {
+          query = { email: req.query.email };
+        }
+        const result = await offerPropertiesCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
