64be54bffffc006f6bf344a095dfb92498af52c8
Mahfuja Ahsan Muna <121941219+mahfuja5768@users.noreply.github.com>
2023-11-28 20:32:19 +0600

    changr route

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -38,6 +39,7 @@ async function run() {
     const soldPropertiesCollection = client
       .db("realEstate")
       .collection("soldProperties");
+    const paymentCollection = client.db("realEstate").collection("payments");
 
     //jwt
     app.post("/jwt", async (req, res) => {
@@ -120,6 +122,19 @@ async function run() {
     });
 
     //get single properties
+    app.get("/properties2/:id", verifyToken, async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        console.log('mmmm----------->',query)
+        const result = await propertiesCollection.findOne(query);
+         console.log(result)
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    //get single properties
     app.get("/properties/:id", verifyToken, async (req, res) => {
       try {
         const id = req.params.id;
@@ -163,7 +178,8 @@ async function run() {
         }
         const result = await reviewsCollection
           .find(query)
-          .sort({ date: -1 }).limit(4)
+          .sort({ date: -1 })
+          .limit(4)
           .toArray();
         res.send(result);
       } catch (error) {
@@ -172,7 +188,7 @@ async function run() {
     });
 
     //get reviews
-    app.get("/reviews",verifyToken, async (req, res) => {
+    app.get("/reviews", verifyToken, async (req, res) => {
       try {
         let query = {};
         if (req.query?.email) {
@@ -412,7 +428,9 @@ async function run() {
       try {
         const email = req.params.email;
         const query = { email: email };
+        // console.log(query)
         const result = await usersCollection.find(query).toArray();
+        // console.log(result)
         res.send(result);
       } catch (error) {
         console.log(error);
@@ -494,6 +512,37 @@ async function run() {
       res.send(result);
     });
 
+    //payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, "aaa---->");
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    //payment related api
+    app.post("/payments", async (req, res) => {
+      try {
+        const payment = req.body;
+        const paymentResult = await paymentCollection.insertOne(payment);
+        const offerPropertyId = payment.offerPropertyId;
+        const updateResult = await offerPropertiesCollection.updateOne(
+          { _id: new ObjectId(offerPropertyId) },
+          { $set: { status: "accepted" } }
+        );
+        res.send(updateResult);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -516,7 +565,6 @@ app.get("/", (req, res) => {
 app.listen(port, (req, res) => {
   console.log(`Dream property is running on ${port}`);
 });
-
 
 /* fuzzy-school.surge.sh */
 /* astonishing-doll.surge.sh */
