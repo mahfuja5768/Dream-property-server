3f3cbb0db67e03df34b9d786a59b0e7761a51488
Mahfuja Ahsan Muna <121941219+mahfuja5768@users.noreply.github.com>
2024-01-01 00:17:15 +0600

    change port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
